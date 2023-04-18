bd0cce2b870fe389b21323464d4db6d58d7382a9
mounika <mounika107@gmail.com>
2023-04-18 10:29:03 +0000

    keypair name chnaged

--- deployneeds/main.tf
+++ deployneeds/main.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "test-server" {
   ami           = "ami-02eb7a4783e7e9317" 
   instance_type = "t2.micro" 
-  key_name = "pemfile"
+  key_name = "manu"
   vpc_security_group_ids= ["sg-0e5fd0e80c68fc9cb"]
   connection {
     type     = "ssh"
     user     = "ubuntu"
-    private_key = file("./pemfile.pem")
+    private_key = file("./manu.pem")
     host     = self.public_ip
   }
   provisioner "remote-exec" {
