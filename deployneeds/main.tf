eb75327f85d63623b974dac9db525479ccb08dd1
mounika <mounika107@gmail.com>
2023-04-18 16:48:36 +0000

    ec2-user changed

--- deployneeds/main.tf
+++ deployneeds/main.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "test-server" {
-  ami           = "ami-02eb7a4783e7e9317" 
+  ami           = "ami-07d3a50bd29811cd1" 
   instance_type = "t2.micro" 
   key_name = "manu"
   vpc_security_group_ids= ["sg-0e5fd0e80c68fc9cb"]
   connection {
     type     = "ssh"
-    user     = "ubuntu"
+    user     = "ec2-user"
     private_key = file("./manu.pem")
     host     = self.public_ip
   }
