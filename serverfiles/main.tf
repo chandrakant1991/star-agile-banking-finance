74c592d4d8e6628e0edfe3c85790e386b5aafad0
noor1718 <120811117+noor1718@users.noreply.github.com>
2023-04-27 18:48:51 +0530

    Update main.tf

--- serverfiles/main.tf
+++ serverfiles/main.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "test-server" {
   ami           = "ami-0c768662cc797cd75" 
   instance_type = "t2.micro" 
-  key_name = "msknoor"
+  key_name = "msk"
   vpc_security_group_ids= ["sg-018f2433993460c5d"]
   connection {
     type     = "ssh"
     user     = "ec2-user"
-    private_key = file("./msnoor.pem")
+    private_key = file("./msk.pem")
     host     = self.public_ip
   }
   provisioner "remote-exec" {
